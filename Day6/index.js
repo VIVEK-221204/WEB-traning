3cad4e136853b6449701273a609ed1f8a3969f9b
VIVEK-221204 <vivek_247076@saitm.ac.in>
2024-12-04 09:49:03 +0530

    Add files via upload

---
+++ Day6/index.js
@@ -0,0 +1,65 @@
+// console.log("-----welcome in js-------");co
+// console.log("------- variable in js--------")
+// const a = 5;
+// let b = 6;
+// var var1 = 5;
+// var var2="variable -2-in- a sting format ";
+// var var3=6758756567468;
+// console.log(var1,var2,var3);
+// var r= a+b;
+// console.log("sum of the numbers :", r);
+
+
+// let var1 = 5;
+// let var2="variable -2-in- a sting format ";
+// let var3=6758756567
+// const a=6;
+// const b=7;
+// const c=8;
+// if(a>b && a>c){
+//     console.log(a);
+// }
+// if(b> a && b>c){
+// console.log(b);
+// }
+// if(c>a&&c>b){
+//     console.log(c);
+// }
+// //loop;
+// for (let i = 0; i<10; i++){
+//     const newValue = i+5;
+//     console.log(newValue);
+// console.log("No. Divided by 3")
+
+
+// for(i=0; i<100;i++){
+//     if (i%3==0){
+//         console.log(i);
+//     }
+
+// }
+
+// console.log("No. Divided by 3 and 5")
+
+// for(i=0; i<100;i++){
+//     if (i%3==0 && i%5 == 0){
+//         console.log(i);
+//     }
+
+// }
+// console.log("No. Divided by 3 and 9")
+
+// for(i=0; i<100;i++){
+//     if (i%3==0 && i%5 == 0 && i%9){
+//         console.log(i);
+//     }
+
+// }
+let i =0;
+let n = 10;
+while(i<n){
+    console.log(i);
+    i=i+1;
+
+
+}
